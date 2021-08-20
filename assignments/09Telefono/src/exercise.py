0c1072143aecf68cec89401e1e2061baef2a2a0a
SofiaAlvarez01 <89089343+SofiaAlvarez01@users.noreply.github.com>
2021-08-20 14:59:38 -0500

    Update exercise.py

--- assignments/09Telefono/src/exercise.py
+++ assignments/09Telefono/src/exercise.py
@@ -1,5 +1,15 @@
 def main():
     #escribe tu código abajo de esta línea
+    import math
+    men=int(input("Dame el número de mensajes:"))
+    meg=float(input("Dame el número de megas:"))
+    minu=int(input("Dame el número de minutos:"))
+    ment=men*.80
+    megt=meg*.80
+    minut=minu*.80
+    total=ment+megt+minut
+    print("El costo mensual es:",total)
+
     #Leer los datos
     pass
 
